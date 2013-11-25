7852c35b6279372660bde0b5a5cc35ac9c174f33
Joshua Melia <joshua.melia@me.com>
2013-11-24 19:54:03 -0800

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Joshua Melia on 11/24/13.
+//  Copyright (c) 2013 Joshua Melia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
